2a09dfa8e8c9e392be6cbcdc31e6914ffadaf5f6
Lavraj333 <lavrajkarna333@gmail.com>
2026-05-09 12:38:50 +0545

    fix import in models.py

--- actresses/models.py
+++ actresses/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from .utils import get_google_image
+from .utils import get_tmdb_profile
 
 class Actress(models.Model):
     name = models.CharField(max_length=200, unique=True)
-    profile_path = models.TextField(blank=True, null=True)  # now stores full URL
+    profile_path = models.TextField(blank=True, null=True)
 
     def save(self, *args, **kwargs):
         if not self.profile_path:
-            image_url = get_google_image(self.name)
+            image_url = get_tmdb_profile(self.name)
             if image_url:
                 self.profile_path = image_url
         super().save(*args, **kwargs)
