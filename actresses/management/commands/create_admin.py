a698694bbdb603ca4da432280f5733d67db21144
Lavraj333 <lavrajkarna333@gmail.com>
2026-05-09 11:43:19 +0545

    add create_admin command

---
+++ actresses/management/commands/create_admin.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = 'Create default superuser'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='admin@glamour.com',
+                password='glamour2025'
+            )
+            self.stdout.write(self.style.SUCCESS('✅ Superuser created!'))
+        else:
+            self.stdout.write('⏭ Superuser already exists, skipping.')
