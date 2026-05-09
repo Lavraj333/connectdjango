91d121a8a6cdbe285c12a09096c839c960585425
Lavraj333 <lavrajkarna333@gmail.com>
2026-05-09 12:07:47 +0545

    fix utils to return full tmdb url

--- actresses/utils.py
+++ actresses/utils.py
@@ -2,25 +2,21 @@ import requests
 from django.conf import settings
 
 TMDB_URL = "https://api.themoviedb.org/3/search/person"
+TMDB_BASE = "https://image.tmdb.org/t/p/h632"
 
 def get_tmdb_profile(name):
-
     params = {
         "query": name,
         "api_key": settings.TMDB_API_KEY
     }
-
     try:
         response = requests.get(TMDB_URL, params=params)
-
         data = response.json()
-
-        # check if results exist
         if "results" in data and len(data["results"]) > 0:
-            return data["results"][0].get("profile_path")
-
+            path = data["results"][0].get("profile_path")
+            if path:
+                return f"{TMDB_BASE}{path}"  # ← return full URL now
         return None
-
     except Exception as e:
         print("TMDB error:", e)
         return None
