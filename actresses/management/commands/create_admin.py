2dc93a5fc42cf829799cf46d1bb0e1103180601a
Lavraj333 <lavrajkarna333@gmail.com>
2026-05-29 09:24:18 +0545

    always recreate admin on deploy

--- actresses/management/commands/create_admin.py
+++ actresses/management/commands/create_admin.py
@@ -6,16 +6,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
-        if User.objects.filter(username='admin').exists():
-            # Reset password if user already exists
-            user = User.objects.get(username='admin')
-            user.set_password('glamour2025')
-            user.save()
-            self.stdout.write(self.style.SUCCESS('✅ Password reset to glamour2025'))
-        else:
-            User.objects.create_superuser(
-                username='admin',
-                email='admin@glamour.com',
-                password='glamour2025'
-            )
-            self.stdout.write(self.style.SUCCESS('✅ Superuser created!'))
+        User.objects.all().delete()
+        User.objects.create_superuser(
+            username='admin',
+            email='admin@glamour.com',
+            password='glamour2025'
+        )
+        self.stdout.write(self.style.SUCCESS('✅ Admin created!'))
